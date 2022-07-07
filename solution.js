e7e467b2e4bda729a24601ef09c91d699fedf79a
dci-student <alinacuznetov@gmail.com>
2022-07-07 12:12:02 +0200

    arrays pt1

--- solution.js
+++ solution.js
@@ -1,1 +1,41 @@
+//1 
+var euroCities = ["Chisinau", "Sofia", "Belgrade", "Tirana", "Athens"];
+console.log(euroCities);
+var secondCity = euroCities[1];
+console.log(secondCity);
+
+//2
+euroCities[0]="Berlin";
+console.log(euroCities);
+
+//3
+console.log(euroCities.length);
+
+//4 
+euroCities(pop);
+console.log(euroCities);
+
+//5 
+euroCities.push("Budapest");
+console.log(euroCities);
+
+//6 
+var asianCities = ["Mumbai", "Hanoi","Kathmandu", "New Dehli", "Almaty"];
+console.log(asianCities);
+
+//7
+var worldCities = euroCities.concat(asianCities);
+console.log(worldCities);
+
+//8
+console.log(worldCities.reverse());
+
+//9
+console.log(euroCities.splice(2,1));
+
+//10
+var chosenCities = asianCities
+
+
+
 
